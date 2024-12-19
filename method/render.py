b89cb54cceb572d5c0d197d2a35980781dec7028
gizmore <gizmore@wechall.net>
2024-12-19 02:09:16 +0100

    'async fixes'

--- method/render.py
+++ method/render.py
@@ -7,7 +7,6 @@ from gdo.base.Application import Application
 from gdo.base.GDT import GDT
 from gdo.base.Method import Method
 from gdo.base.Util import Random
-from gdo.core.GDT_Char import GDT_Char
 from gdo.core.GDT_Raw import GDT_Raw
 from gdo.ui.GDT_Color import GDT_Color
 from gdo.ui.GDT_Font import GDT_Font
@@ -20,9 +19,8 @@ class render(Method):
     def gdo_trigger(self) -> str:
         return ''
 
-    def gdo_parameters(self) -> [GDT]:
+    def gdo_parameters(self) -> list[GDT]:
         return [
-            # GDT_Char('static').minlen(5),
             GDT_Width('width').initial('240').not_null(),
             GDT_Height('height').initial('64').not_null(),
             GDT_Color('bg').initial('#ffffff00').not_null(),
@@ -52,16 +50,13 @@ class render(Method):
 
         # Draw each letter with random rotation and position
         for i, char in enumerate(captcha_text):
-            # Create a separate image for each character to rotate it
             char_image = Image.new('RGBA', (50, 50), bg)
             char_draw = ImageDraw.Draw(char_image)
             char_draw.text((10, 5), char, font=font, fill=fg)
 
-            # Rotate the character image
             angle = random.randint(-30, 30)
             rotated_char_image = char_image.rotate(angle, expand=1, fillcolor=bg)
 
-            # Paste the rotated character onto the main image
             w5 = int((width - 10) / 5)
             x = 10 + i * w5 + random.randint(-15, 5)
             y = random.randint(-5, 5)
@@ -78,13 +73,11 @@ class render(Method):
 
         image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
 
-        # Save or display the image
         buffer = BytesIO()
         image.save(buffer, format='gif')
         buffer.seek(0)
         Application.header('Content-Type', 'image/gif')
         return GDT_Raw('result').val(buffer.read())
-        # return image.show()  # Or use image.save("captcha.png") to save
 
     def get_font(self):
         fonts = GDT_Font.FONTS
